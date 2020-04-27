59c701a58e23d50ceb7fa8d11eb99d20f345995c
Vladimir-Beliy <vovaazick@gmail.com>
2020-04-27 15:42:18 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b5f5aa038ea1c441f5f00fa1d4be5aa3.js"
+  "./precache-manifest.7bda26c554986ccab25d900eb54d6bb3.js"
 );
 
 self.addEventListener('message', (event) => {
